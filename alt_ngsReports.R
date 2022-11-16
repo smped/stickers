3bbed9cb38ab2ea4d2e5a742159983872ca7f1f6
Steve Pederson <stephen.pederson.au@gmail.com>
2022-11-17 01:49:36 +1030

    Tried alternate ngsReports sticker

---
+++ alt_ngsReports.R
@@ -0,0 +1,42 @@
+library(hexSticker)
+
+
+## PNG
+sticker(
+    subplot = "alt_ngs_background.png",
+    s_x=1.05, s_y=1.3, s_width=.52, s_height = 0.52,
+    package="ngsReports",
+    p_size=24,
+    p_color = "black",
+    p_y = 0.65,
+    # p_family = "Helvetica",
+    filename="alt_ngsReports_sticker.png",
+    h_fill = "white",
+    h_color = "black"
+)
+
+sticker(
+    subplot = "alt_ngs_background.png",
+    s_x=1.05, s_y=1.3, s_width=.52, s_height = 0.52,
+    package="ngsReports",
+    p_size=8,
+    p_color = "black",
+    p_y = 0.65,
+    # p_family = "Helvetica",
+    filename="alt_ngsReports_sticker.pdf",
+    h_fill = "white",
+    h_color = "black"
+)
+
+sticker(
+    subplot = "alt_ngs_background.png",
+    s_x=1.05, s_y=1.3, s_width=.52, s_height = 0.52,
+    package="ngsReports",
+    p_size=8,
+    p_color = "black",
+    p_y = 0.65,
+    # p_family = "Helvetica",
+    filename="alt_ngsReports_sticker.svg",
+    h_fill = "white",
+    h_color = "black"
+)
